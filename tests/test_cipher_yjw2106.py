dc9ef53a185099b0dd4c2bda118152479491ddf0
yjw224 <yjw224@nyu.edu>
2020-11-14 23:07:22 -0500

    updates

--- tests/test_cipher_yjw2106.py
+++ tests/test_cipher_yjw2106.py
@@ -1,5 +1,6 @@
-from cipher_yjw2106 import __version__
 from cipher_yjw2106 import cipher_yjw2106
-
-def test_version():
-    assert __version__ == '0.1.0'
+    
+def test_cipher():
+    assert cipher('animal', 0) == 'animal', "Expected outcome: animal"
+    assert cipher('animal', 2) == 'cpkocn', "Expected outcome: cpkocn"
+    assert cipher('sum', 10) == 'DFw', "Expected outcome: DFw"
